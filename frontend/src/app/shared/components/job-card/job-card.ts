71aec5cf823a6b4d10188205729965ce4c6aa347
mraihanghani <mraihanghaniii@gmail.com>
2026-03-16 08:21:56 +0700

    feat: add job card

---
+++ frontend/src/app/shared/components/job-card/job-card.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-job-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './job-card.html',
+})
+export class JobCard{}
