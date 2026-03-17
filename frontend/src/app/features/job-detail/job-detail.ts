fd6efe08cb750009864f73facceec02006f1b633
mraihanghani <mraihanghaniii@gmail.com>
2026-03-17 09:06:49 +0700

    feature: add job detail

---
+++ frontend/src/app/features/job-detail/job-detail.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { JobService } from '../../core/services/job';
+import { Job } from '../../core/mock/job.mock';
+import { JobCard } from '../../shared/components/job-card/job-card';
+
+@Component({
+  selector: 'app-job',
+  standalone: true,
+  imports: [RouterLink, JobCard],
+  templateUrl: './job.html',
+})
+export class JobDetail implements OnInit {
+  job: Job | null = null;
+  otherJobs: Job[] = [];
+
+  constructor(
+    private route: ActivatedRoute,
+    private jobService: JobService,
+  ) {}
+
+  ngOnInit() {
+    this.route.params.subscribe((params) => {
+      const id = params['id'];
+      this.jobService.getJobById(id).subscribe((job) => {
+        this.job = job;
+      });
+      this.jobService.getJobs().subscribe((jobs) => {
+        this.otherJobs = jobs.filter((j) => j.id !== id).slice(0, 3);
+      });
+    });
+  }
+}
