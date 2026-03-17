bb798a2d4b790f93529ee8464289d7f28ded759c
mraihanghani <mraihanghaniii@gmail.com>
2026-03-17 09:05:48 +0700

    feature: add footer

---
+++ frontend/src/app/shared/components/footer/footer.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  standalone: true,
+  imports: [],
+  templateUrl: './footer.html',
+})
+export class Footer {
+  currentYear = new Date().getFullYear();
+}
