29a191b8370617869735e3d78f547550922a3ce6
mraihanghani <mraihanghaniii@gmail.com>
2026-03-17 09:05:36 +0700

    feature: make static data into dyanmic

--- frontend/src/app/shared/components/job-card/job-card.ts
+++ frontend/src/app/shared/components/job-card/job-card.ts
@@ -1,9 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { Job } from '../../../core/mock/job.mock';
 
 @Component({
   selector: 'app-job-card',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './job-card.html',
 })
-export class JobCard{}
+export class JobCard {
+  @Input() job!: Job;
+
+  getSkillsLabel(skills: string[]): string {
+    const maxShow = 2;
+    if (skills.length <= maxShow) {
+      return skills.join(', ');
+    }
+    const shown = skills.slice(0, maxShow).join(', ');
+    const remaining = skills.length - maxShow;
+    return `${shown}, +${remaining} more`;
+  }
+}
