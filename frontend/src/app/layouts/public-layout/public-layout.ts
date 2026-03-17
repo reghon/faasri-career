86105d3368936c1adc061ed61a091936bf04320d
mraihanghani <mraihanghaniii@gmail.com>
2026-03-17 09:06:12 +0700

    chore: add footer

--- frontend/src/app/layouts/public-layout/public-layout.ts
+++ frontend/src/app/layouts/public-layout/public-layout.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from '../../shared/components/navbar/navbar';
+import { Footer } from '../../shared/components/footer/footer';
 
 @Component({
   selector: 'app-public-layout',
   standalone: true,
-  imports: [RouterOutlet, Navbar],
+  imports: [RouterOutlet, Navbar, Footer],
   templateUrl: './public-layout.html',
 })
 export class PublicLayout {}
