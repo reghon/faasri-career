081ce376bb8ef421bdf77a22066af67463d61056
mraihanghani <mraihanghaniii@gmail.com>
2026-03-26 10:54:40 +0700

    feat(auth): add jwt utility functions

---
+++ backend/app/utils/jwt.util.ts
@@ -0,0 +1,27 @@
+import jwt, { SignOptions } from "jsonwebtoken";
+import { config } from "../configurations/env";
+
+export interface JwtPayloadCustom {
+  userId: string;
+  role?: string;
+}
+
+export const signAccessToken = (payload: JwtPayloadCustom): string => {
+  return jwt.sign(payload, config.jwt.secret, {
+    expiresIn: config.jwt.expiresIn,
+  } as SignOptions);
+};
+
+export const signRefreshToken = (payload: JwtPayloadCustom): string => {
+  return jwt.sign(payload, config.jwt.refreshSecret, {
+    expiresIn: config.jwt.refreshExpiresIn,
+  } as SignOptions);
+};
+
+export const verifyAccessToken = (token: string): JwtPayloadCustom => {
+  return jwt.verify(token, config.jwt.secret) as JwtPayloadCustom;
+};
+
+export const verifyRefreshToken = (token: string): JwtPayloadCustom => {
+  return jwt.verify(token, config.jwt.refreshSecret) as JwtPayloadCustom;
+};
