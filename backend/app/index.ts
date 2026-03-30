5e0e570952a03dc054fd2e3542398070af808556
Ubuntu <mraihanghani@Bismillah.neo.internal>
2026-03-30 09:27:27 +0700

    fix(cors): fix CORS url

--- backend/app/index.ts
+++ backend/app/index.ts
@@ -18,7 +18,7 @@ const PORT = Number(process.env.PORT) || config.app.port;
 
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || "http://localhost:4200",
+    origin: process.env.CLIENT_URL || "https://career.aigra.id",
     credentials: true,
   }),
 );
