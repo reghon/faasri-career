a3571577af43c7097ac2214b82ba15e404965215
mraihanghani <mraihanghaniii@gmail.com>
2026-04-02 15:14:55 +0700

    fix(db): remove users id reference

--- backend/app/db/migrations/V02__create_table_roles.sql
+++ backend/app/db/migrations/V02__create_table_roles.sql
@@ -4,9 +4,6 @@ CREATE TABLE IF NOT EXISTS roles (
   description VARCHAR(255),
   is_active BOOLEAN NOT NULL DEFAULT true,
   created_at TIMESTAMP NOT NULL DEFAULT NOW(),
-  created_by UUID REFERENCES users(id),
   updated_at TIMESTAMP NOT NULL DEFAULT NOW(),
-  updated_by UUID REFERENCES users(id),
-  deleted_at TIMESTAMP,
-  deleted_by UUID REFERENCES users(id)
+  deleted_at TIMESTAMP
 );
