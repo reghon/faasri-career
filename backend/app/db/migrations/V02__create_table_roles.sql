e5f9c1958bed856e6da61b6b2cc59c8107e1168f
mraihanghani <mraihanghaniii@gmail.com>
2026-04-06 14:38:07 +0700

    chore(db-roles): add code and superadmin column

--- backend/app/db/migrations/V02__create_table_roles.sql
+++ backend/app/db/migrations/V02__create_table_roles.sql
@@ -1,7 +1,9 @@
 CREATE TABLE IF NOT EXISTS roles (
   id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+  code varchar(100) NOT NULL UNIQUE,
   name VARCHAR(255) NOT NULL UNIQUE,
   description VARCHAR(255),
+  is_superadmin BOOLEAN NOT NULL DEFAULT false,
   is_active BOOLEAN NOT NULL DEFAULT true,
   created_at TIMESTAMP NOT NULL DEFAULT NOW(),
   updated_at TIMESTAMP NOT NULL DEFAULT NOW(),
