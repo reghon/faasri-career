5a9d3aaca75f726061d40dc4f5813527828120a1
mraihanghani <mraihanghaniii@gmail.com>
2026-04-06 15:14:36 +0700

    feat(applicant-certification): add model and service

--- frontend/src/app/domain/applicant/certification/certification.service.ts
+++ frontend/src/app/domain/applicant/certification/certification.service.ts
@@ -1,31 +1,41 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { map } from 'rxjs';
+import { map, Observable } from 'rxjs';
 import { API_ENDPOINTS } from '../../../core/config/api.config';
-import { Certification } from './certification.model';
+import { Certification, CertificationPayload } from './certification.model';
 
-@Injectable({ providedIn: 'root' })
+interface ApiResponse<T> {
+  message: string;
+  data: T;
+}
+
+@Injectable({
+  providedIn: 'root',
+})
 export class CertificationService {
-  constructor(private http: HttpClient) {}
+  constructor(private readonly http: HttpClient) {}
 
-  getAll() {
+  getAll(): Observable<Certification[]> {
     return this.http
-      .get<{ data: Certification[] }>(API_ENDPOINTS.applicant.certifications)
-      .pipe(map((res) => res.data));
+      .get<ApiResponse<Certification[]>>(API_ENDPOINTS.applicant.certifications)
+      .pipe(map((response) => response.data));
   }
 
-  create(data: Partial<Certification>) {
-    return this.http.post<{ data: Certification }>(API_ENDPOINTS.applicant.certifications, data);
+  create(payload: CertificationPayload): Observable<Certification> {
+    return this.http
+      .post<ApiResponse<Certification>>(API_ENDPOINTS.applicant.certifications, payload)
+      .pipe(map((response) => response.data));
   }
 
-  update(id: string, data: Partial<Certification>) {
-    return this.http.put<{ data: Certification }>(
-      `${API_ENDPOINTS.applicant.certifications}/${id}`,
-      data,
-    );
+  update(id: string, payload: CertificationPayload): Observable<Certification> {
+    return this.http
+      .put<ApiResponse<Certification>>(`${API_ENDPOINTS.applicant.certifications}/${id}`, payload)
+      .pipe(map((response) => response.data));
   }
 
-  delete(id: string) {
-    return this.http.delete(`${API_ENDPOINTS.applicant.certifications}/${id}`);
+  delete(id: string): Observable<void> {
+    return this.http
+      .delete<ApiResponse<null>>(`${API_ENDPOINTS.applicant.certifications}/${id}`)
+      .pipe(map(() => void 0));
   }
 }
