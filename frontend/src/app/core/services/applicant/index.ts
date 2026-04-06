9613a85e74852b1ff2ae5d015f97327fbca1f7ea
mraihanghani <mraihanghaniii@gmail.com>
2026-04-06 14:48:13 +0700

    refactor(frontend): delete untracked rename

--- frontend/src/app/core/services/applicant/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './applicant.model';
-export * from './applicant_profile.service';
-export * from './work_experience.service';
-export * from './education.service';
-export * from './certification.service';
-export * from './language.service';
-export * from './technical_skill.service';
