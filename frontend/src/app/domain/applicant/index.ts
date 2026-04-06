49e71f85756c72cc74a697876c964ce4fa4f909c
mraihanghani <mraihanghaniii@gmail.com>
2026-04-06 15:13:39 +0700

    chore(applicant_master): add domain exports

---
+++ frontend/src/app/domain/applicant/index.ts
@@ -0,0 +1,20 @@
+export * from './applicant_master.model';
+export * from './applicant_master.service';
+
+export * from './applicant_profile/applicant_profile.model';
+export * from './applicant_profile/applicant_profile.service';
+
+export * from './education/education.model';
+export * from './education/education.service';
+
+export * from './certification/certification.model';
+export * from './certification/certification.service';
+
+export * from './language/language.model';
+export * from './language/language.service';
+
+export * from './technical_skill/technical_skill.model';
+export * from './technical_skill/technical_skill.service';
+
+export * from './work_experience/work_experience.model';
+export * from './work_experience/work_experience.service';
