6e1ff5b940044ee7374baffe20b85b7398d9e558
mraihanghani <mraihanghaniii@gmail.com>
2026-04-10 11:09:05 +0700

    refactor(language): ensure softDelete sets is_active to FALSE

--- backend/app/modules/applicant/languages/language.queries.ts
+++ backend/app/modules/applicant/languages/language.queries.ts
@@ -32,7 +32,7 @@ export const languageQueries = {
   `,
   softDelete: `
     UPDATE languages
-    SET deleted_at = NOW(), deleted_by = $3, updated_at = NOW(), updated_by = $3
+    SET deleted_at = NOW(), deleted_by = $3, updated_at = NOW(), updated_by = $3, is_active = FALSE 
     WHERE id = $1
       AND applicant_profile_id = $2
       AND deleted_at IS NULL
