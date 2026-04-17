2b681592152e5accbb4271c24734cd2a7653353e
mraihanghani <mraihanghaniii@gmail.com>
2026-04-17 11:02:38 +0700

    feat(master-data): add barrel export file

---
+++ frontend/src/app/domain/master-data/index.ts
@@ -0,0 +1,20 @@
+export * from './department/department.model';
+export * from './department/department.service';
+
+export * from './education-level/education-level.model';
+export * from './education-level/education-level.service';
+
+export * from './employment-type/employment-type.model';
+export * from './employment-type/employment-type.service';
+
+export * from './job-category/job-category.model';
+export * from './job-category/job-category.service';
+
+export * from './job-location/job-location.model';
+export * from './job-location/job-location.service';
+
+export * from './job-status/job-status.model';
+export * from './job-status/job-status.service';
+
+export * from './work-mode/work-mode.model';
+export * from './work-mode/work-mode.service';
