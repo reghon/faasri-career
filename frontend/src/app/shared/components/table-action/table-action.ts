a70b76a2fcba4d2983b3adc43290fa0b7dc18316
mraihanghani <mraihanghaniii@gmail.com>
2026-04-29 16:56:48 +0700

    feat(table-action): add custom action option

--- frontend/src/app/shared/components/table-action/table-action.ts
+++ frontend/src/app/shared/components/table-action/table-action.ts
@@ -1,6 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 
+export type TableActionItem = {
+  label: string;
+  icon?: string;
+  class?: string;
+  disabled?: boolean;
+  value: string;
+};
+
 @Component({
   selector: 'app-table-action',
   standalone: true,
@@ -12,7 +20,16 @@ export class TableActionComponent {
   @Input() showEdit = false;
   @Input() showDelete = false;
 
+  @Input() useDropdown = false;
+  @Input() actions: TableActionItem[] = [];
+
   @Output() view = new EventEmitter<void>();
   @Output() edit = new EventEmitter<void>();
   @Output() delete = new EventEmitter<void>();
+  @Output() actionClick = new EventEmitter<string>();
+
+  onActionClick(action: TableActionItem) {
+    if (action.disabled) return;
+    this.actionClick.emit(action.value);
+  }
 }
