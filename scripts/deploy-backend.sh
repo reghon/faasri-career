5e9ffa44403b27b278ce90248e45a307958f5e49
mraihanghani <mraihanghaniii@gmail.com>
2026-06-04 09:51:48 +0700

    remove unneccessary file

--- scripts/deploy-backend.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-echo "[INFO] Deploying backend..."
-
-pm2 restart faasri-backend || pm2 start backend/dist/index.js --name faasri-backend
-pm2 save
-
-echo "[OK] Backend deployed."
